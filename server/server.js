f0164de0c99861e8295a490a60693ddbb9966d92
sporthq <nowak.sebastian991@gmail.com>
2024-01-07 21:15:08 +0100

    testing server

--- server/server.js
+++ server/server.js
@@ -14,9 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.json('Hello');
-});
+
 app.use('/api/books', booksRoutes);
 app.use('/api/users', userRoutes);
 
