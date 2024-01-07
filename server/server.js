c2f4446b1053ffefc922016907c82425b5f6826e
sporthq <nowak.sebastian991@gmail.com>
2024-01-07 20:52:18 +0100

    server settings

--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+	res.json('Hello');
+});
 app.use('/api/books', booksRoutes);
 app.use('/api/users', userRoutes);
 
@@ -23,14 +26,14 @@ app.get('/api/config/google', (req, res) => res.send(process.env.GOOGLE_CLIENT_I
 
 const port = 5000;
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if(process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client/dist')));
+// if(process.env.NODE_ENV === 'production') {
+// 	app.use(express.static(path.join(__dirname, '/client/dist')));
 
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
-}
+// 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html')))
+// }
 app.get('/', (req, res) => {
 	res.json({ message: 'Api is running' });
 });
