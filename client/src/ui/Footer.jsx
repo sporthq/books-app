4b23243eec97fb18bbc98e7cacbd72dd631801c2
Sebastian Nowak <nowak.sebastian991@gmail.com>
2024-09-23 16:52:55 +0200

    small fixed

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -3,19 +3,19 @@ import styled from 'styled-components';
 
 const FooterStyled = styled.footer`
 	display: flex;
-	
+
 	justify-content: center;
 	gap: 0.45rem;
 	grid-column: 2/-1;
 	padding-top: 4.8rem;
 	padding-bottom: 2.4rem;
 	text-align: center;
-	
+
 	@media only screen and (max-width: 75em) {
 		grid-column: 1/-1;
 	}
 
-	@media only screen and (max-width: 28em){
+	@media only screen and (max-width: 28em) {
 		flex-direction: column;
 	}
 `;
@@ -28,7 +28,9 @@ export default function Footer() {
 	return (
 		<FooterStyled>
 			&copy; {new Date().getFullYear()} Created by{' '}
-			<TextColor to={'https://www.nowakart.pl'} target='_blank' rel='noreferrer noopener'>www.nowakart.pl 🪼</TextColor>
+			<TextColor to={'https://www.nowakart.com.pl'} target='_blank' rel='noreferrer noopener'>
+				www.nowakart.com.pl 🪼
+			</TextColor>
 		</FooterStyled>
 	);
 }
